664b28be8d2b4cc5b2c7f0487ea594de7ebf7939
Sarah Schaller <sarcsch@aol.com>
2017-10-18 14:29:29 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,16 +12,14 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params["pet"])
-    if !params["pet"]["name"].empty?
-      @pet.owner << Owner.create(name: params["owner"]["name"])
+    if !params["owner"]["name"].empty?
+      owner = Owner.create(name: params["owner"]["name"])
+    else
+      owner = Owner.find_by_id(params["pet"]["owner_id"])
     end
+    owner.pets << @pet
     @pet.save
     redirect to "pets/#{@pet.id}"
-  end
-
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
   end
 
   get '/pets/:id' do
@@ -29,12 +27,21 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+   get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
-      @pet.owners << Owner.create(name: params["owner"]["name"])
-    end 
+      owner = Owner.create(name: params["owner"]["name"])
+    else
+      owner = Owner.find_by_id(params["pet"]["owner_id"])
+    end
+    owner.pets << @pet if !owner.pets.include?(@pet)
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
